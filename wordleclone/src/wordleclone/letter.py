a4e4217e7d1d3f8e8f1d36644cec369cc3a3dd06
Jared Dantis <jareddantis@gmail.com>
2022-02-06 18:47:32 +0800

    Create custom WordleLetter Label class

---
+++ wordleclone/src/wordleclone/letter.py
@@ -0,0 +1,24 @@
+from toga import Label
+from toga.style import Pack
+from .constants import COLOR_GREEN
+
+class WordleLetter(Label):
+    def __init__(self, letter: str):
+        super().__init__(letter, style=Pack(
+            flex=1,
+            text_align='center'
+        ))
+
+        self._green = False
+    
+    @property
+    def green(self):
+        return self._green
+    
+    @green.setter
+    def green(self, value: bool):
+        self._green = value
+        if value:
+            self.style.update(font_weight='bold', color=COLOR_GREEN)
+        else:
+            self.style.update(font_weight='normal', color='black')
